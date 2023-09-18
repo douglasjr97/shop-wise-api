4df8e71acafa82d8c52bd167cdc5378c96087a6b
douglasjr97 <falcao.douglas97@gmail.com>
2023-09-17 21:01:03 -0300

    refactor: user post

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
-    @Get()
-    async getAllUsers() {
-        return JSON.stringify({ test: 'its working' })
+    @Post()
+    async createUser(@Body() createUser: CreateUserDto) {
+        return {
+            ...createUser,
+            password: undefined
+        }
     }
 }
